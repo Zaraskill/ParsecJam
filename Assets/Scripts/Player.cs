3f1f0c8f54df5f688705f9548b9e18f94cae4ef6
6meon <malaretsimeon@gmail.com>
2020-11-02 15:17:20 +0100

    Sprites folder and player movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed = 5f;
+
+    [Header("Component")]
+    private Rigidbody2D _rigidbody;
+    Vector2 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody.MovePosition(_rigidbody.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
