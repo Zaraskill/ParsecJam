c3e1c1b1bab4a3933aff9b1e771403229a2e05f2
Corentin <corentin.lelong77@gmail.com>
2020-11-03 15:54:36 +0100

    Game manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Rewired;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,10 +10,20 @@ public class GameManager : MonoBehaviour
 
     private GAME_STATE stateGame = GAME_STATE.dayTime;
 
+    private int numberDays;
+    private int day = 0;
+    private List<int> playersBoss;
+    private int[] playersScoreFinal;
+    private int[] playersScore;
+    private Player[] listPlayers;
+    private int boss;
+
     public float timeDay = 120f;
+    public float timeVote = 120f;
     public int numberMissionsByDay = 10;
 
     private float timeDayLeft;
+    private float timeVoteLeft;
     private int numberMissionsDone = 0;
 
     private void Awake()
@@ -23,12 +34,27 @@ public class GameManager : MonoBehaviour
         }
         instance = this;
         DontDestroyOnLoad(gameObject);
+
+
+        numberDays = ReInput.controllers.joystickCount;
+        day = 1;
+        playersBoss = new List<int>();
+        playersScoreFinal = new int[numberDays];
+        playersScore = new int[numberDays];
+        listPlayers = FindObjectsOfType<Player>();
     }
 
     // Start is called before the first frame update
     void Start()
     {
         numberMissionsDone = numberMissionsByDay;
+        timeDayLeft = timeDay;
+        timeVoteLeft = timeVote;
+
+        int random = Random.Range(0, numberDays);
+        //Code player dire qu'il est boss
+        playersBoss.Add(random);
+        boss = random;
     }
 
     // Update is called once per frame
@@ -37,20 +63,77 @@ public class GameManager : MonoBehaviour
         switch (stateGame)
         {
             case GAME_STATE.dayTime:
+                timeDayLeft -= Time.deltaTime;
+                if (timeDayLeft <= 0)
+                {
+                    //bloque tout ce qui était en train d'être fait
+                    SwitchStateGame(GAME_STATE.votingTime);
+                }
                 break;
             case GAME_STATE.votingTime:
+                timeVoteLeft -= Time.deltaTime;
+                if (timeVoteLeft <= 0)
+                {
+                    //Enlève l'UI
+                    if (day == numberDays)
+                    {
+                        SwitchStateGame(GAME_STATE.results);
+                    }
+                    else
+                    {
+                        SwitchStateGame(GAME_STATE.dayTime);
+                        day++;
+                    }
+                }
                 break;
             case GAME_STATE.results:
+                //Afficher meilleur boss
                 break;
         }
     }
 
     public void MissionDone(bool isSuccess)
     {
-
+        if (isSuccess)
+        {
+            ////missions + 1 
+            //if(numberMissionsDone == numberMissionsByDay)
+            //{
+            //    //fin jour
+            //}
+        }
     }
 
+    private void SwitchStateGame(GAME_STATE newState)
+    {
+        switch (stateGame)
+        {
+            case GAME_STATE.dayTime:
+                timeDayLeft = Time.deltaTime;
+                int random;
+                do
+                {
+                    random = Random.Range(0, numberDays);
+                } while (playersBoss.Contains(random));
+                //Code player dire qu'il est boss
+                playersBoss.Add(random);
+                boss = random;
+                break;
+            case GAME_STATE.votingTime:
+                int score = 0;
+                foreach(int value in playersScore)
+                {
+                    score += value;
+                }
+                playersScoreFinal[boss] = score;
 
+                timeVoteLeft = Time.deltaTime;
+                // Affichage UI résultats
+                break;
+            case GAME_STATE.results:
+                break;
+        }
+    }
 }
 
 public enum GAME_STATE
