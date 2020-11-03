b29d951564ce9bf7ba3500b7cbca991c1ec328fb
6meon <malaretsimeon@gmail.com>
2020-11-03 11:46:56 +0100

    Interactable objects + Rewired maps

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Rewired;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,21 +12,36 @@ public class Player : MonoBehaviour
     private Rigidbody2D _rigidbody;
     Vector2 movement;
 
+    private Rewired.Player mainPlayer;
+
     // Start is called before the first frame update
     void Start()
     {
+        mainPlayer = ReInput.players.GetPlayer("Player0");
         _rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        movement.x = mainPlayer.GetAxis("HorizontalMove");
+        movement.y = mainPlayer.GetAxis("VerticalMove");
     }
 
     private void FixedUpdate()
     {
         _rigidbody.MovePosition(_rigidbody.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Interactable"))
+        {
+            if(mainPlayer.GetButtonDown("Submit"))
+            {
+                // Lancement mission
+                Debug.Log("Coucou");
+            }
+        }
     }
 }
